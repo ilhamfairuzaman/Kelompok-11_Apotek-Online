ba8eedb5a091a2210467c0be5b9f3b1bf4415fec
Ilham Fairuzaman <ilhamfairuz58@gmail.com>
2021-05-31 19:32:28 +0700

    Update 1.4

--- Apotek Online.py
+++ Apotek Online.py
@@ -3,7 +3,7 @@
 
 import json
 import datetime
-# ini adalah mahakarya
+
 # buka json
 stokbarang = open('datastok.json','r')
 
@@ -17,32 +17,47 @@ totalpb = []
 
 
 def struk():
-
+    print('')
+    print('=' * 30)
+    print('SELAMAT DATANG DI APOTEK SUSET')
+    print('=' * 30) 
     print("Lengkapi identitas dibawah")
     nama_pembeli = input("Masukkan nama pembeli : ")
+
     # Memilih Metode
     carabayar = input("""\nMetode Pembayaran
 1. Online Payment
 2. Bayar Ditempat (cash)
 Pilih (1/2)\n >> """)
     hargatot = sum(totalpb)
+
+    # UI Nota
     print('')
-    print('=' * 15)
-    print('KWITANSI APOTEK SUSAH SEHAT')
-    print('=' * 15)    
+    print('=' * 30)
+    x = 'NOTA PEMBAYARAN APOTEK SUSET'
+    print(x.center(30))
+    print('=' * 30)    
     print('')
-    print('Tgl\t:',datetime.datetime.now())
+    print('Tanggal\t\t:',datetime.datetime.now())
     print('Atas nama\t:', nama_pembeli)
     print('-' * 15)
     lst = len(jumlahb)
     i = 0
-    print('Nama\tJumlah\tHarga\tTotal')
+    print('Nama\t\tJumlah\tHarga\tTotal')
     while i < lst:
         print(f'{namab[i]}\t{jumlahb[i]}\t{hargab[i]}\t{hargab[i]*jumlahb[i]}')
         i += 1
     print('-' * 15)
     print('Harga Total\t:', hargatot)
-    print(f"Keterangan\t:{'Lunas' if carabayar == '1' else 'Belum Dibayar'}")
+    print(f"Keterangan\t: {'Lunas' if carabayar == '1' else 'Belum Dibayar'}")
+    print('')
+    print('=' * 30)
+    x = 'MOHON UNTUK MEMBAWA NOTA SAAT MELAKUKAN PENGAMBILAN OBAT'
+    print(x.center(30))
+    print('=' * 30)
+
+    # kembali ke menu utama
+    beli_pembeli() 
 #   pass
     i = 0
     n = len(idb)
@@ -62,25 +77,40 @@ Pilih (1/2)\n >> """)
         i+=1
 
 def cek_stok():
+    print('')
+    print('=' * 30)
+    print('STOK YANG TERSEDIA APOTEK SUSET')
+    print('=' * 30)  
     with open('datastok.json') as f:
         data = json.load(f)
     i = 1
+
+    # Showing stok
     for stok in data['List']:
-        print(f"[{i}] {stok['nama']} \t: {stok['harga']} \t {stok['stok']}")
+        print(f"[{i}] {stok['nama']} \t: {stok['harga']} \t{stok['stok']}")
         i += 1
 
-    tanya_beli = str(input('Apakah anda ingin membeli (y/n)'))
+    # Tanya
+    tanya_beli = str(input('Apakah anda ingin membeli (y/n) >> '))
     if tanya_beli == 'y':
         beli_pembeli()
     elif tanya_beli == 'n':
         menu_pembeli()
 
 def beli_pembeli():
+    print('')
+    print('=' * 30)
+    print('SELAMAT DATANG DI APOTEK SUSET')
+    print('=' * 30) 
     fjason = open('datastok.json','r')
     data = json.load(fjason)
     temp = data['List']
+
+    #Showing stok
     for indeks in range(len(temp)):
         print("[%d] %s %s %s" % (indeks,temp[indeks]['nama'],temp[indeks]['harga'],temp[indeks]['stok']))
+
+    # Inputting    
     idbeli = int(input('Masukkan ID barang yang mau dibeli >> '))
     jumlahbeli = int(input('Masukkan jumlah barang yang mau dibeli >> '))
     
@@ -121,61 +151,70 @@ def konsultasi():
     for keluh in keluhan :
         print(keluh)
 
-    tanya= (input('Apakah keluhan anda ada dalam daftar ?(Y/N)\n:'))
+    tanya= (input('Apakah keluhan anda ada dalam daftar ?(Y/N)\n: '))
     if tanya == 'Y':
         #menanyakan apakah keluhan 
         keluhan_yang_kamurasain = int(input('Masukkan keluhan penyakit yang anda rasakan (1/2/3/4/5/6/7/8/9) \n:'))
         if keluhan_yang_kamurasain == 1:
-            print("\nObat yang sesuai dengan keluhan Anda adalah Microlax Apotek kami obat untuk keluhan penyakit anda tersebut")
+            print("\nObat yang sesuai dengan keluhan Anda adalah Microlax")
         elif keluhan_yang_kamurasain == 2:
-            print("\nObat yang sesuai dengan keluhan Anda adalah Paracetamol Apotek kami obat untuk keluhan penyakit anda tersebut")
+            print("\nObat yang sesuai dengan keluhan Anda adalah Paracetamol")
         elif keluhan_yang_kamurasain == 3:
-            print("\nObat yang sesuai dengan keluhan Anda adalah Ibuprofen Apotek kami obat untuk keluhan penyakit anda tersebut")
+            print("\nObat yang sesuai dengan keluhan Anda adalah Ibuprofen")
         elif keluhan_yang_kamurasain == 4:
-            print("\nObat yang sesuai dengan keluhan Anda adalah Amoxcicilin Apotek kami obat untuk keluhan penyakit anda tersebut")
+            print("\nObat yang sesuai dengan keluhan Anda adalah Amoxcicilin")
         elif keluhan_yang_kamurasain == 5:
-            print("\nObat yang sesuai dengan keluhan Anda adalah  Kortikosteroid Apotek kami obat untuk keluhan penyakit anda tersebut")
+            print("\nObat yang sesuai dengan keluhan Anda adalah Kortikosteroid")
         elif keluhan_yang_kamurasain == 6:
-            print("\nObat yang sesuai dengan keluhan Anda adalah Penisilin Apotek kami obat untuk keluhan penyakit anda tersebut")
+            print("\nObat yang sesuai dengan keluhan Anda adalah Penisilin")
         elif keluhan_yang_kamurasain == 7:
-            print("\nObat yang sesuai dengan keluhan Anda adalah Neurobion Apotek kami obat untuk keluhan penyakit anda tersebut")
+            print("\nObat yang sesuai dengan keluhan Anda adalah Neurobion")
         elif keluhan_yang_kamurasain == 8:
-            print("\nObat yang sesuai dengan keluhan Anda adalah Gentian Violet Apotek kami obat untuk keluhan penyakit anda tersebut")
+            print("\nObat yang sesuai dengan keluhan Anda adalah Gentian Violet")
         elif keluhan_yang_kamurasain == 9:
-            print("\nObat yang sesuai dengan keluhan Anda adalah Insto Apotek kami obat untuk keluhan penyakit anda tersebut")
+            print("\nObat yang sesuai dengan keluhan Anda adalah Insto")
         else:
-            pass
+            print('Mohon ulangi masukan')
+            konsultasi()
+        tanya = input('Apakah anda ingin membeli obatnya? (y/n) >> ')
+        if tanya == 'y':
+            beli_pembeli()
+        elif tanya =='n':
+            menu_pembeli()
     elif tanya == 'N':
         print("\nMohon maaf, apotek kami belum bisa melayani keluhan penyakit tersebut\nkami sangat menyarankan untuk menghubungi nomor rumah sakit yang sesuai dengan kota anda, berikut adalah nomor rumah sakit di setiap provinsi :\n ")
         list_rs=[
         'RSUP dr Kariadi, Semarang.Telepon: (024) 8413476',
-        'RSUD KRMT Wongsonegoro, Kota Semarang.Telepon: (024) 6711500',
-        'RSUD Tugurejo, Semarang.Telepon: (024) 7605297',
-        'RSU Sultan Agung, Semarang.Telepon: (024) 6580019',
-        'RSU St Elizabeth, Semarang.Telepon: (024) 8310035',
-        'RSU Telogorejo, Semarang.Telepon: (024) 86466000', 
-        'RSU Columbia Asia, Semarang.Telepon: (024) 7629999',
-        'RSU Tk III Bhakti Wira Tamtama, Kota Semarang.Telepon: (024) 3555944',
-        'RSU Bhayangkara, Semarang.Telepon: (024) 6720675',
-        'RSUD Salatiga.Telepon: (0298) 324074',
-        'RS Paru Dr Ario Wirawan, Salatiga.Telepon: (0298) 326130',
-        'RSU Tk IV 04.07.03 dr Asmir, SalatigaTelepon: (0298) 326045',
+        'RSUD KRMT Wongsonegoro, Kota Semarang. Telepon: (024) 6711500',
+        'RSUD Tugurejo, Semarang. Telepon: (024) 7605297',
+        'RSU Sultan Agung, Semarang.T elepon: (024) 6580019',
+        'RSU St Elizabeth, Semarang. Telepon: (024) 8310035',
+        'RSU Telogorejo, Semarang. Telepon: (024) 86466000', 
+        'RSU Columbia Asia, Semarang. Telepon: (024) 7629999',
+        'RSU Tk III Bhakti Wira Tamtama, Kota Semarang. Telepon: (024) 3555944',
+        'RSU Bhayangkara, Semarang. Telepon: (024) 6720675',
+        'RSUD Salatiga. Telepon: (0298) 324074',
+        'RS Paru Dr Ario Wirawan, Salatiga. Telepon: (0298) 326130',
+        'RSU Tk IV 04.07.03 dr Asmir, Salatiga. Telepon: (0298) 326045',
         'RSUD Dr H Soewondo, Kendal.Telepon: (0294) 381433',
         'RSUD Ambarawa Telepon: (0298)591020',
-        'RSUD Sunan Kalijaga Demak Telepon:(0291)685018',
+        'RSUD Sunan Kalijaga Demak. Telepon:(0291)685018',
         'RSUD dr R Soedjati, Grobogan.Telepon:(0292) 421004']
         for rs in list_rs:
             print(rs)
 
 def menu_pembeli():
+    print('')
+    print('=' * 30)
     print('SELAMAT DATANG DI APOTEK SUSET')
+    print('=' * 30)
     choice = int((input('''
     Selamat datang, pelanggan:
-    [1] Cek Stok
-    [2] Beli stok
-    [3] Konsultasi
-    [4] Keluar
-    >> ''')))
+[1] Cek Stok
+[2] Beli stok
+[3] Konsultasi
+[4] Kembali ke menu utama
+>> ''')))
 
     if choice == 1:
         cek_stok()
@@ -190,24 +229,37 @@ def menu_pembeli():
         menu_pembeli()
 
 def penjual_stok():
+    print('')
+    print('=' * 30)
+    print('STOK YANG TERSEDIA APOTEK SUSET')
+    print('=' * 30)  
     with open('datastok.json') as f:
         data = json.load(f)
     i = 1
+
+    # Showing stok 
     for stok in data['List']:
-        print(f"[{i}] {stok['nama']} : {stok['harga']}")
+        print(f"[{i}] {stok['nama']} : {stok['harga']} : {stok['stok']}")
         i += 1
     menu_penjual()
 
     # pass
 
 def tambah_stok_baru():
-    print('SELAMAT DATANG DI APOTEK SUSET')
+    print('')
+    print('=' * 30)
+    print('MENAMBAH STOK APOTEK SUSET')
+    print('=' * 30)
     print('Berikut stok barang yang tersedia')
     fjason = open('datastok.json', 'r')
     data = json.load(fjason)
     temp = data['List']
+
+    # Showing stok
     for indeks in range(len(temp)):
-        print("[%d] %s" % (indeks, temp[indeks]['nama']))
+        print("[%d] %s %s" % (indeks, temp[indeks]['nama'], temp[indeks]['stok']))
+
+    # Tanya    
     n = int((input('Masukkan jumlah barang yang akan ditambahkan >> ')))
     i = 0
     while i < n:
@@ -230,9 +282,15 @@ def tambah_stok_baru():
         menu_penjual()
     # pass
 
+def tambah_stok_lama():
+    pass
+
 def kurangi_stok():
     ## space buat ngisi ##
-    print('SELAMAT DATANG DI APOTEK SUSET')
+    print('')
+    print('=' * 30)
+    print('MENGURANGI STOK APOTEK SUSET')
+    print('=' * 30)
     fjason = open('datastok.json','r')
     data = json.load(fjason)
     temp = data['List']
@@ -247,19 +305,31 @@ def kurangi_stok():
     # pass
 
 def menu_penjual():
+    print('')
+    print('=' * 30)
     print('SELAMAT DATANG DI APOTEK SUSET')
+    print('=' * 30)
     choice = int((input('''
     Selamat datang, admin:
-    [1] Cek Stok
-    [2] Tambah stok
-    [3] Kurangi stok
-    [4] Keluar
-    >> ''')))
+[1] Cek Stok
+[2] Tambah stok
+[3] Kurangi stok
+[4] Kembali ke menu utama
+>> ''')))
 
     if choice == 1:
         penjual_stok()
     elif choice == 2:
-        tambah_stok_baru()
+        tanya = int(input('''Jenis stok yang ingin ditambah
+        [1] Stok Lama
+        [2] Stok Baru\n>> '''))
+        if tanya == 1:
+            tambah_stok_lama()
+        elif tanya ==2:
+            tambah_stok_baru()
+        else:
+            print('Masukkan Ulang pilihan !!!')
+            menu_penjual()
     elif choice == 3:
         kurangi_stok()
     elif choice == 4:
@@ -278,18 +348,24 @@ def login_penjual():
             menu_penjual()
         else:
             print('Password salah !!!')
+            login_penjual()
     else:
         print('Username Salah !!!')
+        login_penjual()
     # pass
 
 def show_menu():
+    print('')
+    print('=' * 30)
     print('SELAMAT DATANG DI APOTEK SUSET')
+    print('=' * 30)    
     choice = int((input('''
     Anda Masuk Sebagai:
-    [1] Penjual
-    [2] Pembeli
-    [3] Keluar
-    >> ''')))
+
+[1] Penjual
+[2] Pembeli
+[3] Keluar
+>> ''')))
 
     if choice == 1:
         login_penjual()
